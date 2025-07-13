1cc2b71f734d8c13c372b4ae90e066756b7608fb
saocodon <32429212+saocodon@users.noreply.github.com>
2025-07-13 19:17:44 +0700

    RevisitToBool

---
+++ NhakhoaMyNgoc/Converters/RevisitToBoolConverter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NhakhoaMyNgoc.Converters
+{
+    public class RevisitToBoolConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length < 2) return false;
+
+            var revisit = values[0] as DateTime?;
+            var visit = values[1] as DateTime?;
+
+            if (!revisit.HasValue || !visit.HasValue)
+                return false;
+
+            return visit.Value.AddYears(1) >= revisit;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture)
+        {
+            // === PLACEHOLDER ===
+            var isChecked = (bool)value;
+            // mặc định (fallback)
+            DateTime? visit = DateTime.Now;
+
+            if (isChecked)
+                return new object[] { visit.Value.AddDays(1), visit };
+            else
+                return new object[] { null, visit };
+        }
+    }
+}
